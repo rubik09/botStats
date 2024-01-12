90de87a9d39d9123b9e455e271ac73a4829104a5
DenisLokhtin <denis.lokhtin@gmail.com>
2024-01-12 12:18:54 +0600

    add admins repository

--- src/admins/admins.module.ts
+++ src/admins/admins.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AdminsService } from './admins.service';
 import { AdminsController } from './admins.controller';
-import { AdminsRepository } from './admins.repository';
+import {Admins} from "./entity/admins";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {AdminsRepository} from "./admins.repository";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Admins])],
+  controllers: [AdminsController],
   providers: [AdminsService, AdminsRepository],
-  controllers: [AdminsController]
 })
 export class AdminsModule {}
--- src/admins/admins.repository.ts
+++ src/admins/admins.repository.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Admins} from "./entity/admins";
+import {Repository} from "typeorm";
 
 @Injectable()
-export class AdminsRepository {}
+export class AdminsRepository {
+    constructor(
+        @InjectRepository(Admins)
+        private readonly adminsRepository: Repository<Admins>,
+    ) {
+    }
+
+    async findOneByEmail(email: string): Promise<Admins> {
+        return await this.adminsRepository.findOne({
+            where: {email},
+        });
+    }
+
+    async findOneById(id: number): Promise<Admins> {
+        return await this.adminsRepository.findOne({
+            where: {id},
+        });
+    }
+}
