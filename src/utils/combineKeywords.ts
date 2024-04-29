4e51a0b1d29bb79134c9abd274a8dcda7ae1c0c0
Yura Kozyrenko <yura.mogilev@mail.ru>
2024-04-29 15:47:48 +0300

    fix combineKeywords

--- src/utils/combineKeywords.ts
+++ src/utils/combineKeywords.ts
@@ -2,19 +2,15 @@ import { TUniqueActivities } from './types';
 import { Keyword } from '../keywords/entity/keywords.entity';
 
 export const combineKeywords = (keywords: Keyword[]) => {
-  console.log(keywords);
   return Object.values(
     keywords.reduce((uniqueActivities: TUniqueActivities, keyword: Keyword) => {
       const { activity, count } = keyword;
-      if (uniqueActivities[activity]) {
-        uniqueActivities[activity].count += count;
-        uniqueActivities[activity].keywords.push(keyword);
-      } else {
-        uniqueActivities[activity] = {
-          ...keyword,
-          keywords: [keyword],
-        };
-      }
+      uniqueActivities[activity]
+        ? ((uniqueActivities[activity].count += count), uniqueActivities[activity].keywords.push(keyword))
+        : (uniqueActivities[activity] = {
+            ...keyword,
+            keywords: [keyword],
+          });
       return uniqueActivities;
     }, {}),
   );
