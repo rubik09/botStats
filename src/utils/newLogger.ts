da027b06f3f0c6ed0788b623635f700c64a1a41e
Yura Kozyrenko <yura.mogilev@mail.ru>
2024-05-23 11:17:26 +0300

    info-message

--- src/utils/newLogger.ts
+++ src/utils/newLogger.ts
@@ -6,7 +6,7 @@ export default class NewLogger extends Logger {
   async info(message: string) {
     console.log(`\x1b[33m Info: ${message} \x1b[0m`);
 
-    if (message === 'The server closed the connection') {
+    if (message === 'The server closed the connection while sending' || message === 'connection closed') {
       const appInstance = await app;
       const userSessionService = appInstance.get(UserSessionService);
       userSessionService.reconnectAllUserSessions();
