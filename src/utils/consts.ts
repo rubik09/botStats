42507a460371badcde2f36c707942d9032497ca6
Yura Kozyrenko <yura.mogilev@mail.ru>
2024-06-24 09:19:43 +0300

    const createClient

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -7,3 +7,9 @@ export const enum setupSteps {
 export const cronTimezone = 'Europe/Moscow';
 
 export const MESSAGES_CONNECTION_CLOSED = ['The server closed the connection while sending'];
+
+export const createCLientConfig = {
+  CONNECTION_RETRIES: 5,
+  SEQUENTIAL_UPDATES: true,
+  FLOOD_SLEEP_THRESHOLD: 300,
+};
--- src/utils/createClient.ts
+++ src/utils/createClient.ts
@@ -1,6 +1,7 @@
 import { TelegramClient } from 'telegram';
 import { StringSession } from 'telegram/sessions';
 
+import { createCLientConfig } from './consts';
 import { ICreateClient } from './interfaces';
 import NewLogger from './newLogger';
 
@@ -8,13 +9,13 @@ export const createClient = async ({ logSession, apiId, apiHash }: ICreateClient
   try {
     const stringSession = new StringSession(logSession);
     const client = new TelegramClient(stringSession, Number(apiId), apiHash, {
-      connectionRetries: 5,
-      sequentialUpdates: true,
+      connectionRetries: createCLientConfig.CONNECTION_RETRIES,
+      sequentialUpdates: createCLientConfig.SEQUENTIAL_UPDATES,
+      floodSleepThreshold: createCLientConfig.FLOOD_SLEEP_THRESHOLD,
       baseLogger: new NewLogger(),
     });
 
     const isConnect = await client.connect();
-    client.floodSleepThreshold = 300;
 
     if (!isConnect) {
       throw new Error(`client not connected with apiId: ${apiId} and apiHash: ${apiHash}`);
