cab10cd3e2e87d057c4f43803ec5db09ef96af0d
DenisLokhtin <denis.lokhtin@gmail.com>
2024-07-24 13:08:43 +0600

    add from and to query parameters

--- src/calculatedStats/dto/getStats.dto.ts
+++ src/calculatedStats/dto/getStats.dto.ts
@@ -1,5 +1,5 @@
 import { PickType } from '@nestjs/mapped-types';
-import { IsNumber, Min } from 'class-validator';
+import { IsISO8601, IsNumber, Min } from 'class-validator';
 
 import { CalculatedStatDto } from './calculatedStat.dto';
 
@@ -11,4 +11,10 @@ export class GetStatsDto extends PickType(CalculatedStatDto, ['username']) {
   @IsNumber()
   @Min(1)
   limit: number;
+
+  @IsISO8601()
+  from?: CalculatedStatDto['createdAt'];
+
+  @IsISO8601()
+  to?: CalculatedStatDto['createdAt'];
 }
